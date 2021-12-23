f8f9bb35b38bd66da43c3e73ae552ea09605c527
1dxy <96384971+1dxy@users.noreply.github.com>
2021-12-23 12:18:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Client } = require('discord.js');
-const client = new Client({intents: 3});
+const { Client } = require('discord.js'); // Importing the Objects from discord.js into our code.
+const client = new Client({intents: 3}); // Creating our client with intents. Intent Calculator: https://ziad87.net/intents/
 
-const config = require('./config.json');
+const config = require('./config.json'); // Importing our data from our config.json to our code.
 
-client.once('ready', () => {
-    console.log(`${client.user.username} is now online!`)
-    client.user.setActivity("This is the Bot Status", {type: "WATCHING"}) // Type could be competing, watching, streaming, listening, and playing.
+client.once('ready', () => { // Creating an event so we know when the bot is ready.
+    console.log(`${client.user.username} is now online!`) // Logs the bots user name is now online when it is online completely.
+    client.user.setActivity("This is the Bot Status", {type: "WATCHING"}) // Setting the status of our bot: Type could be competing, watching, streaming, listening, and playing.
 })
 
 
-client.login(config.token)
+client.login(config.token) // Login your bot with your BOT_TOKEN in our file config.json
